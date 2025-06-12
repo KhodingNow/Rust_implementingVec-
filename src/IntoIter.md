65a8a1a8557348b26a968856ca5d6f1e21fda712
Melezwa26 <phalosenzwa@gmail.com>
2025-06-12 12:56:23 +0200

    Vec adding iterators

---
+++ src/IntoIter.md
@@ -0,0 +1,7 @@
+Moving into iterators - iter and iter_mut have already been written for us, thanks to the magic of Deref.However, there's two interesting iterators that Vec provides that slices can't: into_iter and drain.
+IntoIter consumes the Vec by-value, and can consequently yield its elements by-value. In order to enable this, IntoIter needs to take control of Vec's allocation.
+
+IntoIter needs to be douBleEnded as well, to enable reading from both ends. Reading from the back could just be implemented as calling pop, but reading from the front is harder. We could call remove(0) but that wld be insanely expensive. Instead we'll just use ptr::read to copy values out of either end of the Vec without mutating the buffer at all.
+
+To achieve this, we are to use a very common C idiom for array iteration. We make two pointers, one that oints to the start of the array, and one that points to one-element past the end.When we want an element from one end, we'll read  out the value pointed to at that end and move the pointer over by one. 
+When the two pointers are equal, we know we are done.
--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,7 @@ impl<T> Vec<T> {
             // "Layout::array" checks that the number of bytes is <= usize::MAX,
             // but this is redundant since old_layout.size() <= isize::MAX,
             // so the 'unwrap' should never fail.
-            let new new_layout = Layout::array::<T>(new_cap).unwrap();
+            let new_layout = Layout::array::<T>(new_cap).unwrap();
             (new_cap, new_layout)
         };
 
@@ -57,7 +57,7 @@ impl<T> Vec<T> {
             unsafe { alloc::realloc(old_ptr, old_layout, new_layout.size()) }
         };
 
-        // If allocation fails, new_ptr will be nuull, in which case we abort
+        // If allocation fails, new_ptr will be null, in which case we abort
         self.ptr = match NonNull::new(new_ptr as *mut T) {
             Some(p) => p,
             None => alloc::handle_alloc_error(new_layout),
@@ -77,4 +77,96 @@ impl<T> Vec<T> {
         // Can't fail, we'll OOM first.
         self.len += 1;
     }
-}
+
+    // IntoIter for Vec iteration
+
+    pub struct IntoIter<T> {
+        buf: NonNull<T>,
+        cap: usize,
+        start: *const T,
+        end: *const T,
+    }
+
+    // We end up with this for initialization:
+
+    impl<T> IntoIterator for Vec<T> {
+        type item = T;
+        type IntoIter = IntoIter<T> {
+            // Make sure not to drop Vec since that wld free the buffer
+            let vec = ManuallyDrop::new(self);
+
+            // Can't destructure Vec since it's Drop
+            let ptr = vec.ptr;
+            let cap = vec.cap;
+            let len = vec.len;
+
+            IntoIter {
+                buf: ptr,
+                cap,
+                start: ptr.as_ptr(),
+                end: if cap == 0 {
+                    // can't offset this pointer, its not allocated
+                    ptr.as_ptr()
+                } else {
+                    unsafe { ptr.as_ptr().add(len) }
+                },
+            }
+        }
+    }
+
+    // Here's iterating forward:
+
+    impl<T> Iterator for IntoIter<T> {
+        type Item = T;
+        fn next(&mut self) -> Option<T> {
+            if self.start == self.end {
+                None
+            } else {
+                unsafe {
+                    let result = ptr::read(self.start);
+                    self.start = self.start.offset(1);
+                    Some(result)
+                }
+            }
+        }
+
+        fn size_hint(&self) -> (usize, Option<usize>) {
+            let len = (self.end as usize - self.start as usize)
+                        / mem::size_of::<T>();
+            (len, Some(len))
+        }
+
+    }
+
+    // Here's iterating backWards.
+
+    impl<T> DoubleEndedIterator for IntoIter<T> {
+        fn next_back(&mut self) -> Option<T> {
+            if self.start == self.end {
+                None
+            } else {
+                unsafe {
+                    self.end = self.offset(-1);
+                    Some(ptr::read(self.end))
+                }
+            }
+        }
+    }
+    // IntoIter takes ownership of its allocation, it needs to implement Drop to free it.
+    // However, it also want to implement Drop to any elements it contains that were not yielded.
+
+    impl<T> Drop for IntoIter<T> {
+        fn drop(&mut self) {
+            if self.cap != 0 {
+                // drop any remaining elements
+
+                for _ in &mut *self {}
+                let layout = Layout::array::<T>(self.cap).unwrap();
+                unsafe {
+                    alloc::dealloc(self.buf.as_ptr() as *mut u8, layout);
+                }
+            }
+        }
+    }
+
+} 
